dd126711241ad2c3aba3071316e07a0481042198
Yasaswini <yasha.balu25@gmail.com>
2025-02-20 22:57:41 +0530

    Testimonial Card -second commit. Fixing the router link and adding a link in home page.

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { Link } from "react-router";
+import './style/index.css'
+
+function Home() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <main className='main'>
+            <section className="projects-section">
+                <h3 className='projects-section__heading'>GreatFrontEnd Project Solutions</h3>
+                <ul>
+                    <li><Link to="/testimonial">Testimonial Card</Link></li>
+                </ul>
+            </section>
+        </main>
+    )
+}
+
+export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Home from './Pages/Home'
 import { BrowserRouter, Routes, Route } from 'react-router'
-import Testimonial from './Projects/Testimonial';
+import Testimonial from './Pages/Testimonial';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/testimonial" element={<Testimonial />} />
+        <Route index element={<Home />} />
+        <Route path="testimonial" element={<Testimonial />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
