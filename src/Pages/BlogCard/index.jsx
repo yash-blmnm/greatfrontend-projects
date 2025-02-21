7f33c3ca0e789ba7ce295ab7a5ba1bfadcd4e2f0
Yasaswini <yasha.balu25@gmail.com>
2025-02-21 09:59:24 +0530

    Blog Card - Link style changes and Vercel route fix

--- src/Pages/BlogCard/index.jsx
+++ src/Pages/BlogCard/index.jsx
@@ -4,12 +4,12 @@ import spacejoy from './assets/spacejoy.jpg';
 export default function BlogCard() {
     return <main className="blog-card-main">
         <section className='blog-card'>
-            <div className='blog-card__image' style={{ backgroundImage: `url(${spacejoy})` }}></div>
+            <div className='blog-card__image' style={{ backgroundImage: `url(${spacejoy})` }} alt="Space Joy Unsplash" ></div>
             <div className='blog-card__content'>
                 <div className='blog-card__tag'>Interior</div>
                 <h5 className='blog-card__title'>Top 5 Living Room Inspirations</h5>
                 <p className='blog-card__summary'>Curated vibrants colors for your living, make it pop & calm in the same time.</p>
-                <a className='blog-card__link'>Read more &rarr;	</a>
+                <a src="" className='blog-card__link'>Read more &rarr;	</a>
             </div>
         </section>
 
